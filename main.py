551c4580b4b3d92e2d61c8b12623563995e8f290
mickey-40 <mickeyarnold53@yahoo.com>
2024-09-05 19:45:59 -0500

    Resolved the csv error and now will print the quote with the auther name

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-from bs4 import BeautifulSoup
-import requests
 import csv
+import requests
+from bs4 import BeautifulSoup
 
+# Fetch the webpage
 page_to_scrap = requests.get("http://quotes.toscrape.com")
 soup = BeautifulSoup(page_to_scrap.text, "html.parser")
-quotes = soup.findAll("span", attrs={"class":"text"})
-authors = soup.findAll("small", attrs={"class":"author"})
 
-file = open("scraped_quotes.csv", "w")
-writer.writerow(["Quotes", "Authors"])
+# Find the quotes and authors
+quotes = soup.findAll("span", attrs={"class": "text"})
+authors = soup.findAll("small", attrs={"class": "author"})
 
-for quote, author in zip(quotes, authors):
-  print(quote.text + " - " + author.text)
-  writer.writerow([quote.text, author.text])
+# Open the file in write mode and create a CSV writer instance
+with open("scraped_quotes.csv", "w", newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Quotes", "Authors"])  # Write the header
 
-file.close()
-
+    # Loop through the quotes and authors and write them to the CSV file
+    for quote, author in zip(quotes, authors):
+        print(quote.text + " - " + author.text)
+        writer.writerow([quote.text, author.text])
